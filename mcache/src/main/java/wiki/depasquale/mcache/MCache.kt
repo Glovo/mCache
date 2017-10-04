4789d2cca1cb8d6817538b6085d736aa27d29ce2
viktor de pasquale <neytireyqa@gmail.com>
2017-10-04 20:05:44 +0200

    MCACHE
    - convert main class to object (static) and remove most of unnecessary methods
    - set/get context in more clean fashion

--- mcache/src/main/java/wiki/depasquale/mcache/MCache.kt
+++ mcache/src/main/java/wiki/depasquale/mcache/MCache.kt
@@ -1,71 +1,20 @@
 package wiki.depasquale.mcache
 
-import android.app.*
-import android.content.*
-import wiki.depasquale.mcache.adapters.*
-import wiki.depasquale.mcache.core.*
-import java.lang.ref.*
-import java.util.*
+import android.app.Application
+import android.content.Context
+import java.lang.ref.WeakReference
 
-/**
- * diareuse on 26.03.2017
- */
+object MCache {
 
-class MCache private constructor() {
+  private lateinit var contextReference: WeakReference<Context>
 
-  init {
-    throw RuntimeException("This constructor shall not be used!")
+  @JvmStatic
+  fun with(context: Application) {
+    contextReference = WeakReference(context)
   }
 
-  companion object {
-
-    private val sIOHandlerInstance = HashMap<Class<*>, IOHandler>(0)
-    private var sContext: WeakReference<Context>? = null
-
-    /**
-     * Initializes MCache
-     * IOHandler - [CacheIOHandler]
-     */
-    @JvmStatic
-    fun with(context: Application) {
-      MCache.sContext = WeakReference<Context>(context)
+  internal val context: Context
+    get() {
+      return contextReference.get() ?: throw RuntimeException("Context must not be null.")
     }
-
-    @JvmStatic
-    fun get(): Context? {
-      sContext?.let {
-        return it.get()
-      }
-      return null
-    }
-
-    @JvmStatic
-    fun getIOHandler(cls: Class<out IOHandler>?): IOHandler {
-      var classToRead = cls
-      if (classToRead == null) {
-        classToRead = CacheIOHandler::class.java
-      }
-      if (sIOHandlerInstance.containsKey(classToRead)) {
-        return sIOHandlerInstance[classToRead]!!
-      } else {
-        try {
-          val interfaceType = classToRead.newInstance()
-          sIOHandlerInstance.put(classToRead, interfaceType)
-          return interfaceType
-        } catch (e: InstantiationException) {
-          throw RuntimeException(classToRead.name + " cannot be instantiated.")
-        } catch (e: IllegalAccessException) {
-          throw RuntimeException(classToRead.name + " probably does not contain constructor.")
-        }
-
-      }
-    }
-
-    @JvmStatic
-    @SafeVarargs fun clean(vararg cls: Class<out IOHandler>) {
-      for (handler in cls) {
-        getIOHandler(handler).clean()
-      }
-    }
-  }
 }
