a3090f4c2fd02905b4a01d40ad4f521519a0bd4b
viktor de pasquale <neytireyqa@gmail.com>
2017-10-22 17:45:46 +0200

    MCACHE
    - remove unneeded extension

--- mcache/src/main/java/wiki/depasquale/mcache/Extensions.kt
+++ mcache/src/main/java/wiki/depasquale/mcache/Extensions.kt
@@ -2,20 +2,6 @@ package wiki.depasquale.mcache
 
 import android.util.Base64
 
-internal fun String?.isNumber(): Boolean {
-  this ?: return false
-  return try {
-    /*if (contains(".")) {
-      val f = toFloatOrNull()
-      val d = if (f == null) toDoubleOrNull() else null
-      return f != null || d != null
-    }*/
-    toIntOrNull() != null
-  } catch (e: Exception) {
-    false
-  }
-}
-
 internal fun String.unBase64(): String {
   return String(Base64.decode(toByteArray(), Base64.NO_WRAP))
 }
