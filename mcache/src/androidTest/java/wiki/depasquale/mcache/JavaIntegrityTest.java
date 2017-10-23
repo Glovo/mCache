e9fb3e37f6a00744ed99b0af9f5a9cc5f14c9e33
Viktor De Pasquale <neytireyqa@gmail.com>
2017-10-23 09:26:13 +0200

    MCACHE
    - fix tests for java calls

--- mcache/src/androidTest/java/wiki/depasquale/mcache/JavaIntegrityTest.java
+++ mcache/src/androidTest/java/wiki/depasquale/mcache/JavaIntegrityTest.java
@@ -3,24 +3,29 @@ package wiki.depasquale.mcache;
 import android.support.test.filters.MediumTest;
 import android.support.test.runner.AndroidJUnit4;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-
-/**
- * diareuse on 22.10.2017
- */
 
 @MediumTest
 @RunWith(AndroidJUnit4.class)
 public class JavaIntegrityTest {
 
   @Before
-  public void setup() {
-    Cache
-        .obtain(Cache.class)
-        .build();
+  public void setup() {}
 
-    Cache
-        .give(this)
-        .build();
+  @Test
+  public final void testIntegrity() {
+    try {
+      Cache
+          .obtain(Cache.class)
+          .build();
+
+      Cache
+          .give(this)
+          .build();
+    } catch (Exception e) {
+      assert e instanceof RuntimeException;
+      assert e.getMessage().contains("Context");
+    }
   }
 }
